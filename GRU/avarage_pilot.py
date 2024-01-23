3dd526bdf7a3af03555b0f98161e3821338eaa65
aumat2011 <aumat2011@gmail.com>
2024-01-24 00:47:55 +0300

    Overall Scores

--- GRU/avarage_pilot.py
+++ GRU/avarage_pilot.py
@@ -5,7 +5,10 @@ import numpy as np
 
 #rates = [0.5664, 0.5664, 0.5684, 0.5667, 0.5626] #OVERALL GRU SCORES - BEST FOR EACH FOLD   
 #rates = [0.5671, 0.5657, 0.5673, 0.5662, 0.5626] #OVERALL LSTM0901 SCORES - BEST FOR EACH FOLD
-rates = [0.5668, 0.5650, 0.5660, 0.5674, 0.5637] #OVERALL LSTM1001 SCORES - BEST FOR EACH FOLD  
+#rates = [0.5668, 0.5650, 0.5660, 0.5674, 0.5637] #OVERALL LSTM1001 SCORES - BEST FOR EACH FOLD 
+#rates = [0.5495, 0.5495, 0.5508, 0.5490, 0.5430] #OVERALL L2 1M - BEST FOR EACH FOLD
+#rates = [0.5523, 0.5498, 0.5532, 0.5504, 0.5460] #OVERALL DropOut 1M - BEST FOR EACH FOLD
+rates = [0.5421, 0.5425, 0.5430, 0.5400, 0.5363] #OVERALL L1 1M - BEST FOR EACH FOLD
 
 def lrfn(epoch):
     return rates[epoch]
@@ -34,6 +37,6 @@ plt.xticks(rng, [f'{val:.1f}' for val in rng])
 plt.grid()
 plt.xlabel('Fold', size=14)
 plt.ylabel('Accuracy Rate', size=14)
-plt.title('Overall Success Rate LSTM', size=16)
+plt.title('Overall Success L1', size=16)
 plt.legend()
 plt.show()
